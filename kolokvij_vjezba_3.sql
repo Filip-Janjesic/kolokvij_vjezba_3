a1889b5d90a6198f380b8b4992f66335782c07cc
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-06-07 02:00:49 +0200

    Update kolokvij_vjezba_3.sql

--- kolokvij_vjezba_3.sql
+++ kolokvij_vjezba_3.sql
@@ -1,1 +1,23 @@
 drop database if exists kolokvij_vjezba_3;
+create database kolokvij_vjezba_3;
+use kolokvij_vjezba_3;
+
+create table svekar(
+	sifra int not null primary key auto_increment,
+	novcica decimal (16,8) not null,
+	suknja varchar (46) not null,
+	bojakose varchar(36),
+	prstena int,
+	narukvica int not null,
+	cura int not null
+);
+
+create table cura(
+	sifra int not null primary key auto_increment,
+	dukserica varchar(49),
+	maraka decimal(13,7),
+	drugiputa datetime,
+	majica varchar(49),
+	novcica decimal(15,8),
+	ogrlica int not null
+);
